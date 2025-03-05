93eab6c4314a30dff9ed664f3fd7e924cbffce2b
Christian Blavier <cblavier@gmail.com>
2025-03-05 15:03:44 +0100

    wip

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,34 +1,9 @@
 module.exports = {
-  content: [
-    "./css/**/*.css",
-    "./js/**/*.js",
-    "../lib/**/*.{ex,heex}",
-    "../priv/templates/**/*.eex",
-  ],
   safelist: [
-    { pattern: /^\!?psb-(w|h|m|p)\w?-.+/ },
+    { pattern: /^\!?psb:(w|h|m|p)\w?-.+/ },
     {
       pattern:
-        /^psb-text-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)(-\d\d\d?)?$/,
+        /^psb:text-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)(-\d\d\d?)?$/,
     },
   ],
-  theme: {
-    extend: {
-      minHeight: (theme) => ({
-        ...theme("spacing"),
-      }),
-    },
-  },
-
-  plugins: [
-    require("@tailwindcss/forms")({
-      strategy: "class",
-    }),
-  ],
-  corePlugins: {
-    preflight: false,
-  },
-  important: ".psb",
-  prefix: "psb-",
-  darkMode: "selector",
 };
